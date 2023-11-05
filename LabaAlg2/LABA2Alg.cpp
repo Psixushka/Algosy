ab4b70fc0dd3070278c1a805dd90365e3239d0b5
Psixushka <ulfinkalash0423@bk.ru>
2023-11-05 17:01:09 +0300

    Update LABA2Alg.cpp

--- LabaAlg2/LABA2Alg.cpp
+++ LabaAlg2/LABA2Alg.cpp
@@ -1,268 +1,314 @@
 ﻿#include <iostream>
+#include <vector>
 using namespace std;
 const int RUN = 32;
-
-class dynamic_array
-{
+class DynamicArray {
 private:
-    int size;
-    int* array;
+
+	int* array = NULL;
+	int size;
+	int capacity;
 
 public:
-    dynamic_array()
-    {
-        size = 0;
-        array = NULL;
-        cout << "Введите количество элементов" << endl;
-        cin >> size;
-        array = new int[size];
-        for (int i = 0; i < size; i++) {
-            array[i] = rand() % 199 - 99;
-        }
-    }
-    ~dynamic_array() {
-        delete[] array;
-        array = nullptr;
-    }
+	DynamicArray()
+	{
+		capacity = 1;
+		size = 0;
+		array = new int[capacity];
+	}
 
-    void show()
-    {
-        for (int i = 0; i < size; i++) {
-            cout << array[i] << " ";
-        }
-        cout << endl;
-    }
+	DynamicArray(int capacity)
+	{
+		this->capacity = capacity;
+		array = new int[capacity];
+		size = 0;
+	}
 
-    void insertArray()
-    {
-        show();
-        cout << endl;
-        int Index, Data;
-        int* array2 = new int[size++];
+	int getSize() { return size; }
 
-        cout << "Введите индекс для вставки" << endl;
-        cin >> Index;
-        cout << "Введите значение для вставки" << endl;
-        cin >> Data;
-        for (int i = 0; i < size; i++)
-        {
-            if (i < Index)
-            {
-                array2[i] = array[i];
-            }
+	int getCapacity() { return capacity; }
 
-            if (i >= Index)
-            {
-                array2[i + 1] = array[i];
+	void push_back(int value)
+	{
+		if (size == capacity) {
 
-            }
-            if (i == Index) {
+			growArray();
+		}
 
-                array2[i] = Data;
-            }
+		array[size] = value;
+		size++;
+	}
 
-        }
+	void pop_back()
+	{
+		array[size - 1] = 0;
 
-        for (int i = 0; i < size; i++)
-        {
-            array[i] = array2[i];
-        }
-        for (int i = 0; i < size; i++)
-        {
-            cout << array[i] << " ";
-        }
-        cout << endl;
-    }
+		size--;
+		if (size == (capacity / 2)) {
+			shrinkArray();
+		}
+	}
 
-    void deleteArray()
-    {
-        show();
-        cout << "Введите индекс элемента" << endl;
-        int index;
-        cin >> index;
-        for (int i = index; i < size - 1; i++)
-        {
-            array[i] = array[i + 1];
-        }
-        size--;
+	void growArray()
+	{
+		int* temp = new int[capacity * 2];
 
-        for (int i = 0; i < size; i++)
-        {
-            cout << array[i] << " ";
-        }
-        cout << endl;
-    }
-    int GetElemIndex(int Index, int Data)
-    {
+		capacity = capacity * 2;
+		for (int i = 0; i < size; i++) {
+			temp[i] = array[i];
+		}
 
-        for (int i = 0; i < size; i++)
-        {
-            if (i == Index)
-            {
-                Data = array[i];
-            }
-        }
-
-        return Data;
-    }
-    int GetElemData(int Index, int Data)
-    {
-
-        for (int i = 0; i < size; i++)
-        {
-            if (array[i] == Data)
-            {
-                Index = i;
-            }
-
-        }
-
-        return Index;
-    }
+		delete[] array;
+		array = temp;
+	}
 
 
-    void InsertionSort(int left, int right) {
-        for (int i = left + 1; i <= right; i++) {
-            int temp = array[i];
-            int j = i - 1;
-            while (array[j] > temp && j >= left) {
-                array[j + 1] = array[j];
-                j--;
-            }
-            array[j + 1] = temp;
-        }
-    }
+
+	void shrinkArray()
+	{
+
+		capacity = size;
+		int* temp = new int[capacity];
+
+		for (int i = 0; i < size; i++) {
+			temp[i] = array[i];
+		}
+
+		delete[] array;
+		array = temp;
+	}
+
+	int search(int key)
+	{
+		for (int i = 0; i < size; i++) {
+			if (array[i] == key) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	void insertAt(int index, int value)
+	{
+		if (size == capacity) {
+			growArray();
+		}
+
+		for (int i = size - 1; i >= index; i--) {
+			array[i + 1] = array[i];
+		}
+
+		array[index] = value;
+		size++;
+	}
+
+	void deleteAt(int index)
+	{
+		for (int i = index; i < size; i++) {
+			array[i] = array[i + 1];
+		}
+
+		array[size - 1] = 0;
+		size--;
+
+		if (size == (capacity / 2)) {
+			shrinkArray();
+		}
+	}
+
+	void printArrayDetails()
+	{
+		cout << " Массив : ";
+		for (int i = 0; i < size; i++) {
+			cout << array[i] << " ";
+		}
+		cout << endl;
+		cout << " Кол-во элементов : " << size
+			<< ", capacity :" << capacity << endl;
+	}
+
+	bool isEmpty()
+	{
+		if (size == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	int getMinRun()
+	{
+		int r = 0;
+		int n = size;
+		while (n >= RUN) {
+			r |= (n & 1);
+			n >>= 1;
+		}
+		return n + r;
+	}
+
+	void insertionSort(int left, int right)
+	{
+		for (int i = left + 1; i <= right; i++) {
+			int temp = array[i];
+			int j = i - 1;
+			while (j >= left && array[j] > temp) {
+				array[j + 1] = array[j];
+				j--;
+			}
+			array[j + 1] = temp;
+		}
+	}
 
 
-    void merge(int l, int m, int r)
-    {
+	void merge( int l, int m, int r)
+	{
 
-        int len1 = m - l + 1, len2 = r - m;
-        int* left = new int [len1], *right = new int [len2];
-        for (int i = 0; i < len1; i++)
-            left[i] = array[l + i];
-        for (int i = 0; i < len2; i++)
-            right[i] = array[m + 1 + i];
+		int len1 = m - l + 1, len2 = r - m;
+		int* left = new int [len1], *right = new int [len2];
+		for (int i = 0; i < len1; i++)
+			left[i] = array[l + i];
+		for (int i = 0; i < len2; i++)
+			right[i] = array[m + 1 + i];
 
-        int i = 0, j = 0, k = l;
+		int i = 0;
+		int j = 0;
+		int k = l;
 
-        while (i < len1 && j < len2) {
-            if (left[i] <= right[j]) {
-                array[k] = left[i];
-                i++;
-            }
-            else {
-                array[k] = right[j];
-                j++;
-            }
-            k++;
-        }
+	
+		while (i < len1 && j < len2) {
+			if (left[i] <= right[j]) {
+				array[k] = left[i];
+				i++;
+			}
+			else {
+				array[k] = right[j];
+				j++;
+			}
+			k++;
+		}
 
+		while (i < len1) {
+			array[k] = left[i];
+			k++;
+			i++;
+		}
 
-        while (i < len1) {
-            array[k] = left[i];
-            k++;
-            i++;
-        }
+		
+		while (j < len2) {
+			array[k] = right[j];
+			k++;
+			j++;
+		}
+	}
 
+	void timSort()
+	{
+		int minRun = getMinRun();
+		for (int i = 0; i < size; i += minRun)
+			insertionSort(i, min((i + minRun - 1), (size - 1)));
+		int len = minRun;
+		while (len < size) {
+			for (int start = 0; start < size; start += 2 * len) {
+				int mid = start + len - 1;
+				int end = min((start + 2 * len - 1), (size - 1));
+				merge(start, mid, end);
+			}
+			len *= 2;
+		}
 
-        while (j < len2) {
-            array[k] = right[j];
-            k++;
-            j++;
-        }
-        delete[] left;
-        delete[] right;
-    }
+	}
 
-
-    void timSort()
-    {
-
-        for (int i = 0; i < size; i += RUN)
-            InsertionSort( i, min((i + RUN - 1), (size - 1)));
-
-
-        for (int sized = RUN; sized < size; size = 2 * size) {
-
-
-            for (int left = 0; left < size; left += 2 * size) {
-                int mid = left + size - 1;
-                int right = min((left + 2 * size - 1), (size - 1));
-
-                if (mid < right)
-                    merge(left, mid, right);
-            }
-        }
-    }
+	void printArray(int arr[], int n)
+	{
+		for (int i = 0; i < n; i++)
+			printf("%d  ", arr[i]);
+		printf("\n");
+	}
 };
 
 int main()
 {
-    setlocale(0, "RUS");
-    cout << "Поехали" << endl;
-    dynamic_array my_array;
-    int e;
-    do {
-        cout << "Выберите действие: " << endl;
-        cout << "1) Вставка" << endl;
-        cout << "2) Получение значения по индексу " << endl;
-        cout << "3) Получение значения по значению элемента " << endl;
-        cout << "4) Удаление элемента " << endl;
-        cout << "5) Cортировка методом TimSort " << endl << endl;
-        my_array.show();
-        cin >> e;
-        switch (e)
-        {
-        case 1:
-        {
-            my_array.insertArray();
+	setlocale(LC_ALL, "ru");
+	
+	DynamicArray da;
+	int size, i;
+	cout << " Введите количество элементов массива: " << endl;
+	cin >> size;
+	for (i = 0; i < size; i++) {
+		da.push_back(rand() % 199-99);
+	}
 
-        }
-        break;
-        case 2:
-        {
-            int Index, Data = 0;
-            my_array.show();
-            cout << "Введите индекс элемента котрый хотите получить " << endl;
-            cin >> Index;
+	da.printArrayDetails();
+	int e;
+	do {
+		cout << " Выберите действие: " << endl;
+		cout << "1) Вставка" << endl;
+		cout << "2) Получение индекса элемента " << endl;
+		cout << "3) Удаление элемента по индексу " << endl;
+		cout << "4) Удаление последнего элемента  " << endl;
+		cout << "5) Сортировка списка алгоритмом TimSort  " << endl;
+		cout << "6) Выход " << endl;
 
-            Data = my_array.GetElemIndex(Index, Data);
-            cout << endl << "Ваш элемент = " << Data << endl;
+		cin >> e;
 
-        }
-        break;
-        case 3:
-        {
-            int Index = -5, Data = 0;
-            my_array.show();
-            cout << "Введите элемент котрый хотите получить " << endl;
-            cin >> Data;
-            Index = my_array.GetElemData(Index, Data);
-            if (Index != -5) {
-                cout << endl << "Ваш элемент находится под номером " << Index  << endl;
-            }
-            else
-            {
-                cout << "Элемент не найден!" << endl;
-            }
-        }
-        break;
-        case 4:
-        {
-            my_array.deleteArray();
-        }
-        break;
-        case 5:
-        {
-            my_array.timSort();
-            break;
-        }
-        break;
-        default:cout << "Ошибка! Повторите попытку" << endl;
-            break;
-        }
+		switch (e)
+		{
+		case 1:
+		{
+			int index, value;
+			cout << "Введите индекс , куда необходимо вставить элемент " << endl;
+			cin >> index;
+			cout << "Введите элемент " << endl;
+			cin >> value;
+			da.insertAt(index, value);
+			da.printArrayDetails();
+		}
+		break;
+		case 2:
+		{
+			int value;
+			cout << "Введите элемент который хотите получить " << endl;
+			cin >> value;
+			int index = da.search(value);
+			if (index != -1) {
+				cout << "Element found at index : " << index << endl;
+			}
+			else {
+				cout << "Element not found " << endl;
+			}
+		}
+		break;
+		case 3:
+		{
+			int index;
 
-    } while (e != 6);
+			cout << "Введите индекс элемента, который хотите удалить " << endl;
+			cin >> index;
+			da.deleteAt(index);
+			da.printArrayDetails();
+		}
+		break;
+		case 4:
+		{
+			cout << "Удаление последнего элемента  " << endl;
+			da.pop_back();
+			da.printArrayDetails();
+		}
+		break;
+		case 5:
+		{
+			da.timSort();
+			da.printArrayDetails();
+			break;
+		}
+		case 6:
+		{
+			break;
+		}
+		break;
 
+		}
+	} while (e != 6);
 }
